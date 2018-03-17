c7529838c564f16eb7bbf878f598964788cbdb3d
Carlos ovalle <shrek_day@hotmail.com>
2018-03-17 15:57:08 -0500

    Ajustes

--- src/main/java/com/co/micros/sha_pdf/controller/ShaPdfController.java
+++ src/main/java/com/co/micros/sha_pdf/controller/ShaPdfController.java
@@ -1,5 +1,6 @@
 package com.co.micros.sha_pdf.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletResponse;
@@ -7,6 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import com.co.micros.sha_pdf.Pdf;
 import com.co.micros.sha_pdf.service.IPdfService;
@@ -24,13 +28,24 @@ public class ShaPdfController {
 //	@Path("/generateCSV")
 //	@Produces("text/csv")
 	 @RequestMapping("/generate")
-	public HttpServletResponse generateCsv()
-	{
+	public void generateCsv(HttpServletResponse response) throws IOException {	
+	
 		ArrayList<Pdf> pdfs;
 		pdfs = pdfService.findAll();
 		
-		
+		 // uses the Super CSV API to generate CSV data from the model data
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(),
+                CsvPreference.STANDARD_PREFERENCE);
+
+        String[] header = { "Name", "Sha" };
+
+        csvWriter.writeHeader(header);
+
+        for (Pdf p : pdfs) {
+            csvWriter.write(p, header);
+        }
+
+        csvWriter.close();
 	}
-	
-	
+		
 }
