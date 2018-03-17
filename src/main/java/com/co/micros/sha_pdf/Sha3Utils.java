152d06c574bad542395e4b3d1f44aeed8eb3bb93
Carlos ovalle <shrek_day@hotmail.com>
2018-03-17 15:57:08 -0500

    Ajustes

--- src/main/java/com/co/micros/sha_pdf/Sha3Utils.java
+++ src/main/java/com/co/micros/sha_pdf/Sha3Utils.java
@@ -1,11 +1,25 @@
 package com.co.micros.sha_pdf;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.bouncycastle.jcajce.provider.digest.SHA3;
+import org.bouncycastle.util.encoders.Hex;
+
+import com.google.common.io.ByteStreams;
+
 public class Sha3Utils {
-	
-	static void calculate()
-	{
-		
-		
+
+	static String calculate(File file) throws IOException {
+
+		SHA3.DigestSHA3 digestSHA3 = new SHA3.Digest256();
+		InputStream targetStream = new FileInputStream(file);
+		byte[] bytes = ByteStreams.toByteArray(targetStream);
+		byte[] digest = digestSHA3.digest(bytes);
+
+		return Hex.toHexString(digest);
 	}
 
 }
--- src/main/java/com/co/micros/sha_pdf/service/PdfService.java
+++ src/main/java/com/co/micros/sha_pdf/service/PdfService.java
@@ -1,6 +1,7 @@
 package com.co.micros.sha_pdf.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.net.ftp.FTPFile;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,8 @@ public class PdfService implements IPdfService {
 	public ArrayList<Pdf> findAll() {
 		
 		FTPFile[] files = FtpConnet.getFilesFtp();
+		List<Pdf> pdfs = new ArrayList<Pdf>();
+		
 		
 		for(FTPFile f : files)
 		{
