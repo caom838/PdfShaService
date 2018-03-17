c764407465ebb57a063419f504b1c72fd9e28667
Carlos ovalle <shrek_day@hotmail.com>
2018-03-17 17:14:39 -0500

    Ajustes ultimos
    pruebas

--- src/main/java/com/co/micros/sha_pdf/service/PdfService.java
+++ src/main/java/com/co/micros/sha_pdf/service/PdfService.java
@@ -1,9 +1,12 @@
 package com.co.micros.sha_pdf.service;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.net.ftp.FTPFile;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.stereotype.Service;
 
 import com.co.micros.sha_pdf.FtpConnet;
@@ -21,7 +24,9 @@ public class PdfService implements IPdfService {
 		
 		for(FTPFile f : files)
 		{
-			
+			InputStream iStream=f.retrieveFileStream(f.getName());	
+			File file = File.createTempFile("tmp", null);
+			FileUtils.copyInputStreamToFile(iStream, file);
 		}
 		
 		return null;
